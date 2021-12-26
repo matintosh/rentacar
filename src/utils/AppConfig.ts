691f1d64035c458f29c2ae031a01d1011a05e3aa
matintosh <matiasmartineeez@gmail.com>
2021-12-26 18:54:51 -0300

    Support for ddifferent environments

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -3,11 +3,11 @@ export const AppConfig: {
   title: string;
   description: string;
   locale: string;
-  api_url: string;
+  api_url?: string;
 } = {
   site_name: "Martinez rent a car",
   title: "Martinez Rent a Car",
   description: "Alquila tu auto aqui",
   locale: "es",
-  api_url: "https://polar-ravine-93732.herokuapp.com",
+  api_url: process.env.NEXT_PUBLIC_BASE_URL,
 };
